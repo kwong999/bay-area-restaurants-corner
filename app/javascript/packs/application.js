64711f49167db7dad2bc20f3f3bfb33d03790758
Kin Yat <kwong48@horizon.csueastbay.edu>
2019-12-09 23:11:33 -0800

    root route

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,7 +5,7 @@
 
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
-require("channels")
+require('./entry')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
