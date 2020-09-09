ddafc53642d636e5ab5e9a0e926b24f359f3f5a7
Kin Yat <kwong48@horizon.csueastbay.edu>
2020-09-09 12:13:57 -0700

    Create MainContent component

--- app/javascript/frontend/components/app.jsx
+++ app/javascript/frontend/components/app.jsx
@@ -6,7 +6,7 @@ import Header from './header/header';
 import LoginFormContainer from './session/login_form_container'
 import SignupFormContainer from './session/signup_form_container'
 import ProfileShowContainer from './profile/profile_show_container';
-import BusinessMain from './business/business_main';
+import MainContent from './main_content/main_content';
 
 class App extends React.Component {
   render() {
@@ -17,7 +17,7 @@ class App extends React.Component {
           <AuthRoute exact path='/login' component={LoginFormContainer} />
           <AuthRoute exact path='/signup' component={SignupFormContainer} />
           <Route path='/user/:userId' component={ProfileShowContainer} />
-          <Route path='/' component={BusinessMain} />
+          <Route path='/' component={MainContent} />
         </Switch>
         <footer>
         </footer>
